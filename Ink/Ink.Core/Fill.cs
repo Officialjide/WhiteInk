0b97fc21831b5ec4d432b0eb9e2ac6e0479d934b
James Olayinka <olajamesjide@gmail.com>
2018-04-20 14:12:00 +0100

    created core

---
+++ Ink/Ink.Core/Fill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ink.Core
+{
+    public class Fill
+    {
+        public Fill()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
